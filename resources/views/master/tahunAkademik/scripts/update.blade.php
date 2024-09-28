4f928f132083678d7900ea332deb3691f6c1853a
LuthfiRobit <luthfirobit@gmail.com>
2024-09-28 20:20:15 +0700

    m- controller and view for tahun akademik

---
+++ resources/views/master/tahunAkademik/scripts/update.blade.php
@@ -0,0 +1,17 @@
+<script>
+    function updateData(form) {
+        return {
+            tahun: form.find('#tahun').val(),
+            semester: form.find('#semester').val(),
+            status: form.find('#status').val()
+        };
+    }
+
+    // Meng-handle pengiriman form update
+    handleFormSubmission('#updateForm',
+        '{{ route('master-data.tahun-akademik.update', ':id') }}',
+        'PUT',
+        'Tahun Akademik berhasil diperbarui.',
+        updateData // Inisialisasi data khusus
+    );
+</script>
