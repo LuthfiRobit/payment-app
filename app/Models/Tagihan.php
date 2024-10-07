af1966f5caa5e3d9684e870ccd2b75af57b24979
LuthfiRobit <luthfirobit@gmail.com>
2024-10-07 08:57:42 +0700

    m- add static method tagihanbyta and add fillable sisatagihan

--- app/Models/Tagihan.php
+++ app/Models/Tagihan.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Helpers\AppHelper;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\DB;
 
 class Tagihan extends Model
 {
@@ -23,6 +25,7 @@ class Tagihan extends Model
         'besar_tagihan',
         'besar_potongan',
         'total_tagihan',
+        'sisa_tagihan',
         'status',
         'created_at',
         'updated_at'
@@ -70,5 +73,49 @@ class Tagihan extends Model
     public function transaksi(): HasMany
     {
         return $this->hasMany(Transaksi::class, 'tagihan_ida', 'id_tagihan');
+    }
+
+    /**
+     * Additional method
+     */
+
+    // Method untuk mendapatkan data tagihan berdasarkan tahun akademik
+    public static function tagihanByTa($filters = [])
+    {
+        // Get the active academic year
+        $activeAcademicYear = AppHelper::getActiveAcademicYear();
+
+        // Memulai query untuk mendapatkan data tagihan dan siswa
+        $query = DB::table('tagihan')
+            ->select(
+                'siswa.id_siswa',
+                'siswa.nis',
+                'siswa.nama_siswa',
+                'siswa.kelas',
+                'tagihan.id_tagihan',
+                'tagihan.besar_tagihan',
+                'tagihan.besar_potongan',
+                'tagihan.total_tagihan',
+                'tagihan.status',
+                'tahun_akademik.tahun',
+                'tahun_akademik.semester'
+            )
+            ->leftJoin('siswa', 'siswa.id_siswa', '=', 'tagihan.siswa_id')
+            ->leftJoin('tahun_akademik', 'tahun_akademik.id_tahun_akademik', '=', 'tagihan.tahun_akademik_id')
+            ->where('tagihan.tahun_akademik_id', '=', $activeAcademicYear->id_tahun_akademik)
+            ->orderBy('tagihan.created_at', 'DESC');
+
+        // Filter berdasarkan status jika ada
+        if (!empty($filters['filter_status'])) {
+            $query->where('tagihan.status', $filters['filter_status']);
+        }
+
+        // Filter berdasarkan kelas jika ada
+        if (!empty($filters['filter_kelas'])) {
+            $query->where('siswa.kelas', $filters['filter_kelas']);
+        }
+
+        // Mengurutkan berdasarkan tanggal dibuat dan mengembalikan hasil
+        return $query->get();
     }
 }
