aff706b9b9b52c3492c38d7136c5423aef8f20ae
LuthfiRobit <luthfirobit@gmail.com>
2024-10-30 20:32:23 +0700

    m- modify scripts show and store tagihan

--- resources/views/setting/tagihan/scripts/show.blade.php
+++ resources/views/setting/tagihan/scripts/show.blade.php
@@ -1,5 +1,6 @@
 <script>
     $(document).ready(function() {
+        // Menangani tombol detail untuk menampilkan informasi siswa dan tagihan
         $('#example').on('click', '.detail-button', function() {
             var id = $(this).data('id');
             var url = '{{ route('setting.tagihan-siswa.show', ':id') }}'.replace(':id', id);
@@ -14,17 +15,85 @@
                     if (response.data.tagihan_siswa.length > 0) {
                         response.data.tagihan_siswa.forEach(function(tagihan) {
                             $('#tagihanList').append(
-                                '<li class="list-group-item d-flex justify-content-between align-items-center text-uppercase">' +
-                                tagihan.iuran.nama_iuran +
-                                ' <span class="badge light badge-primary badge-pill">' +
-                                tagihan.iuran.besar_iuran + '</span></li>');
+                                '<li class="list-group-item">' +
+                                '<div class="row align-items-center">' +
+                                '<div class="col">' +
+                                '<span class="text-uppercase">' + tagihan.iuran
+                                .nama_iuran + '</span>' + // Nama iuran
+                                '</div>' +
+                                '<div class="col-auto">' +
+                                '<span class="badge light badge-primary badge-pill">' +
+                                tagihan.iuran.besar_iuran + '</span>' +
+                                // Besar iuran
+                                '</div>' +
+                                '<div class="col-auto">' +
+                                '<div class="form-check form-switch">' +
+                                '<input class="form-check-input toggle-iuran" type="checkbox" ' +
+                                (tagihan.status === 'aktif' ? 'checked' : '') +
+                                ' data-id="' + tagihan.id_tagihan_siswa + '">' +
+                                '</div></div></div></li>'
+                            );
                         });
                     } else {
                         $('#tagihanList').append(
                             '<li class="list-group-item align-items-center text-center fw-bold text-uppercase">Belum ada tagihan</li>'
                         );
                     }
+
                     $('#detailTagihanModal').modal('show');
+                }
+            });
+        });
+
+        // Menangani perubahan status iuran
+        $(document).on('change', '.toggle-iuran', function() {
+            var tagihanId = $(this).data('id');
+            var isActive = $(this).is(':checked') ? 'aktif' : 'tidak aktif';
+            var confirmationMessage = isActive === 'aktif' ?
+                'Apakah Anda yakin ingin mengaktifkan tagihan ini?' :
+                'Apakah Anda yakin ingin menonaktifkan tagihan ini?';
+
+            // Tampilkan konfirmasi SweetAlert
+            Swal.fire({
+                title: 'Konfirmasi',
+                text: confirmationMessage,
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Ya, lanjutkan',
+                cancelButtonText: 'Batal'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    // Buat request AJAX untuk memperbarui status iuran
+                    $.ajax({
+                        url: '{{ route('setting.tagihan-siswa.update-status') }}', // Sesuaikan route
+                        method: 'POST',
+                        data: {
+                            id: tagihanId,
+                            status: isActive,
+                            _token: '{{ csrf_token() }}'
+                        },
+                        success: function(response) {
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Sukses!',
+                                text: response.message,
+                                confirmButtonText: 'OK'
+                            });
+                        },
+                        error: function(xhr) {
+                            var errorMessage = xhr.responseJSON.message ||
+                                'Terjadi kesalahan, coba lagi nanti.';
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Error!',
+                                text: errorMessage,
+                                confirmButtonText: 'OK'
+                            });
+                        }
+                    });
+                } else {
+                    // Kembalikan status checkbox ke keadaan semula jika dibatalkan
+                    $(this).prop('checked', !$(this).is(':checked'));
                 }
             });
         });
