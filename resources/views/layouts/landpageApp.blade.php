f45dd4c456e4e228a6c2468d97db5a41f5ef0798
LuthfiRobit <luthfirobit@gmail.com>
2024-11-13 23:28:38 +0700

    ma- layout landpages

---
+++ resources/views/layouts/landpageApp.blade.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Judul Halaman -->
+    <title>
+        Aplikasi Pendaftaran Peserta Didik Baru Madrasah Ibtidaiyah Ihyauddiniyah
+        Desa Kecil Besuk Probolinggo
+    </title>
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Meta begin -->
+    <!-- Set Karakter -->
+    <meta charset="utf-8" />
+    <!-- Mode Rendering -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Penulis Halaman -->
+    <meta name="author" content="Madrasah Ibtidaiyah Ihyauddiniyah" />
+    <!-- Pengindeksan Mesin Pencari -->
+    <meta name="robots" content="index, follow" />
+
+    <!-- Kata Kunci SEO -->
+    <meta name="keywords"
+        content="Pendaftaran Peserta Didik, Madrasah Ibtidaiyah, Desa Kecil Besuk, Probolinggo, Aplikasi Pendaftaran, Sistem Pendaftaran, Pendidikan, Madrasah Ibtidaiyah, Formulir Pendaftaran, Pendaftaran Online, Administrasi Madrasah, Manajemen Data Peserta Didik" />
+
+    <!-- Deskripsi Halaman -->
+    <meta name="description"
+        content="Aplikasi Pendaftaran Peserta Didik Baru untuk Madrasah Ibtidaiyah Ihyauddiniyah di Desa Kecil Besuk Probolinggo. Aplikasi ini memudahkan proses pendaftaran, pengelolaan data peserta didik, dan administrasi dengan desain responsif dan fitur yang user-friendly." />
+
+    <!-- Metadata Open Graph -->
+    <meta property="og:title" content="Aplikasi Pendaftaran Peserta Didik Baru" />
+    <meta property="og:description"
+        content="Aplikasi Pendaftaran Peserta Didik Baru untuk Madrasah Ibtidaiyah Ihyauddiniyah di Desa Kecil Besuk Probolinggo. Aplikasi ini memudahkan proses pendaftaran, pengelolaan data peserta didik, dan administrasi dengan desain responsif dan fitur yang user-friendly." />
+    <meta property="og:image" content="{{ asset('template/social-image.png') }}" />
+
+    <!-- Twitter Card Metadata -->
+    <meta name="twitter:title" content="Aplikasi Pendaftaran Peserta Didik Baru" />
+    <meta name="twitter:description"
+        content="Aplikasi Pendaftaran Peserta Didik Baru untuk Madrasah Ibtidaiyah Ihyauddiniyah di Desa Kecil Besuk Probolinggo. Aplikasi ini memudahkan proses pendaftaran, pengelolaan data peserta didik, dan administrasi dengan desain responsif dan fitur yang user-friendly." />
+    <meta name="twitter:image" content="{{ asset('template/social-image.png') }}" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <!-- Meta end -->
+
+    <!-- Mobile Specific -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('template/images/logo_mi.png') }}" />
+
+    @yield('this-page-style') <!-- Menyertakan Style tambahan dari halaman -->
+
+    <!-- Global style start -->
+    <link href="{{ asset('template/vendor/bootstrap-select/dist/css/bootstrap-select.min.css') }}" rel="stylesheet" />
+    <link class="main-css" href="http://payment-app.test/template/css/style.css" rel="stylesheet" />
+    <!-- Global style end -->
+
+    <!-- This page style start -->
+    <link rel="stylesheet" href="{{ asset('template/css/style-costume.css') }}">
+    <!-- Include Google Font for signature style -->
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+    <!-- This page style end -->
+
+</head>
+
+<body>
+    <!-- Preloader start -->
+    <div id="preloader">
+        <div class="sk-three-bounce">
+            <div class="sk-child sk-bounce1"></div>
+            <div class="sk-child sk-bounce2"></div>
+            <div class="sk-child sk-bounce3"></div>
+        </div>
+    </div>
+    <!-- Preloader end -->
+
+    <!--Main wrapper start-->
+    <div id="main-wrapper">
+
+        @include('layouts.landpageHeader')
+
+        @yield('content')
+
+        @include('layouts.landpageFooter')
+    </div>
+    <!--Main wrapper end-->
+
+    <!-- Global script start -->
+    <script src="{{ asset('template/vendor/global/global.min.js') }}"></script>
+    <script src="{{ asset('template/vendor/bootstrap-select/dist/js/bootstrap-select.min.js') }}"></script>
+    <script src="{{ asset('template/js/custom.min.js') }}"></script>
+    {{-- <script src="{{ asset('template/js/deznav-init.js') }}"></script> --}}
+    @include('layouts.deznav') <!-- Digunakan karna default js tidak bisa load -->
+    @include('layouts.costumeScript') <!-- Digunakan untuk form handler store/update -->
+    <!-- Global script end -->
+
+    <!-- Script theme mode start -->
+    <script>
+        jQuery(document).ready(function() {
+            setTimeout(function() {
+                dezSettingsOptions.version = "light";
+                new dezSettings(dezSettingsOptions);
+                setCookie("version", "light");
+            }, 1500);
+        });
+    </script>
+    <!-- Script theme mode end -->
+
+    <!-- Script token start -->
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
+    <!-- Script token end -->
+
+    @yield('this-page-scripts') <!-- Menyertakan JS tambahan dari halaman -->
+</body>
+
+</html>
---
+++ resources/views/layouts/landpageFooter.blade.php
@@ -0,0 +1,94 @@
+ <!-- Footer start -->
+ <footer class="py-5 px-5 border-top border-3 footer shadow-lg">
+     <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5">
+         <div class="col mb-3">
+             <a href="/" class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none">
+                 <img src="{{ asset('template/images/logo_mi.png') }}" style="height: 75px"
+                     alt="Logo Madrasah Ibtidaiyah Ihyauddiniyah" />
+             </a>
+             <p class="text-body-secondary">
+             <h5>Madrasah Ibtidaiyah Ihyauddiniyah</h5> © 2024
+             </p>
+         </div>
+
+         <!-- Section for other content -->
+         <div class="col mb-3"></div>
+
+         <!-- Useful Links Section -->
+         <div class="col mb-3">
+             <h5>Informasi</h5>
+             <ul class="nav flex-column">
+                 <li class="nav-item mb-2">
+                     <a href="" class="nav-link p-0 text-body-secondary">Tentang Kami</a>
+                 </li>
+                 <li class="nav-item mb-2">
+                     <a href="" class="nav-link p-0 text-body-secondary">Pendaftaran</a>
+                 </li>
+                 <li class="nav-item mb-2">
+                     <a href="" class="nav-link p-0 text-body-secondary">Kontak</a>
+                 </li>
+             </ul>
+         </div>
+
+         <!-- Address Section -->
+         <div class="col mb-3">
+             <h5>Alamat</h5>
+             <ul class="nav flex-column">
+                 <li class="nav-item mb-2">
+                     <p class="nav-link p-0 text-body-secondary">Desa Kecil Besuk, Kabupaten Probolinggo, Jawa
+                         Timur</p>
+                 </li>
+                 <li class="nav-item mb-2">
+                     <p class="nav-link p-0 text-body-secondary">Telepon: +62 123 456 789</p>
+                 </li>
+                 <li class="nav-item mb-2">
+                     <p class="nav-link p-0 text-body-secondary">Email: info@ihyauddiniyah.sch.id</p>
+                 </li>
+             </ul>
+         </div>
+
+         <!-- Quick Links Section -->
+         <div class="col mb-3">
+             <h5>Quick Links</h5>
+             <ul class="nav flex-column">
+                 <li class="nav-item mb-2">
+                     <a href="" class="nav-link p-0 text-body-secondary">Jadwal
+                         Pendaftaran</a>
+                 </li>
+                 <li class="nav-item mb-2">
+                     <a href="" class="nav-link p-0 text-body-secondary">Biaya
+                         Pendaftaran</a>
+                 </li>
+                 <li class="nav-item mb-2">
+                     <a href="" class="nav-link p-0 text-body-secondary">FAQ</a>
+                 </li>
+             </ul>
+         </div>
+     </div>
+
+     <div class="d-flex flex-column flex-sm-row justify-content-between pt-3 border-top border-3">
+         <p>
+             Copyright © Designed &amp; Developed by
+             <a href="https://www.poterteknik.com" target="_blank">PT. POTER TEKNIK INTERNASIONAL</a>
+             <span class="current-year">2024</span>
+         </p>
+         <ul class="list-unstyled d-flex">
+             <li class="ms-3">
+                 <a class="link-body-emphasis" href="https://twitter.com" target="_blank">
+                     <i class="bi bi-twitter"></i> <!-- Ikon Twitter -->
+                 </a>
+             </li>
+             <li class="ms-3">
+                 <a class="link-body-emphasis" href="https://instagram.com" target="_blank">
+                     <i class="bi bi-instagram"></i> <!-- Ikon Instagram -->
+                 </a>
+             </li>
+             <li class="ms-3">
+                 <a class="link-body-emphasis" href="https://facebook.com" target="_blank">
+                     <i class="bi bi-facebook"></i> <!-- Ikon Facebook -->
+                 </a>
+             </li>
+         </ul>
+     </div>
+ </footer>
+ <!-- Footer end -->
