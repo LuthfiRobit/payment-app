17c177c5e5d567646c287c8b1885a80a0c8b3641
LuthfiRobit <luthfirobit@gmail.com>
2025-04-17 19:52:42 +0700

    layouts-landpage

---
+++ resources/views/layouts-landpage/app.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+    <!-- Site Title -->
+    <title>Madrasah Ibtidaiyah Ihyauddiniyah</title>
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Meta -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="author" content="Madrasah Ibtidaiyah Ihyauddiniyah" />
+    <meta name="robots" content="index, follow" />
+
+    <meta name="keywords"
+        content="Pendaftaran Siswa Baru, Madrasah Ibtidaiyah Ihyauddiniyah, Desa Kecik Besuk, Probolinggo, Pendaftaran Sekolah, Aplikasi Pendaftaran, Sistem Pendaftaran, Pendidikan, Sekolah Dasar, Formulir Pendaftaran, Pendidikan Islam, Madrasah Ibtidaiyah, Sistem Informasi Pendaftaran, Aplikasi Pendidikan, UI Modern, Desain Responsif, Aplikasi Web, Sistem Administrasi, Formulir Online, Pendaftaran Mudah, Pengelolaan Pendaftaran" />
+
+    <meta name="description"
+        content="Aplikasi Pendaftaran Siswa Baru untuk Madrasah Ibtidaiyah Ihyauddiniyah di Desa Kecik Besuk Probolinggo. Aplikasi ini memudahkan proses pendaftaran siswa baru dengan sistem yang mudah digunakan, desain responsif, dan fitur yang mendukung administrasi pendaftaran dengan efisien." />
+
+    <meta property="og:title"
+        content="Pendaftaran Siswa Baru Madrasah Ibtidaiyah Ihyauddiniyah Desa Kecik Besuk Probolinggo" />
+    <meta property="og:description"
+        content="Aplikasi Pendaftaran Siswa Baru untuk Madrasah Ibtidaiyah Ihyauddiniyah di Desa Kecik Besuk Probolinggo. Aplikasi ini memudahkan proses pendaftaran siswa baru dengan sistem yang mudah digunakan, desain responsif, dan fitur yang mendukung administrasi pendaftaran dengan efisien." />
+    <meta property="og:image" content="social-image.png" />
+
+    <meta name="format-detection" content="telephone=no" />
+
+    <meta name="twitter:title"
+        content="Pendaftaran Siswa Baru Madrasah Ibtidaiyah Ihyauddiniyah Desa Kecik Besuk Probolinggo" />
+    <meta name="twitter:description"
+        content="Aplikasi Pendaftaran Siswa Baru untuk Madrasah Ibtidaiyah Ihyauddiniyah di Desa Kecik Besuk Probolinggo. Aplikasi ini memudahkan proses pendaftaran siswa baru dengan sistem yang mudah digunakan, desain responsif, dan fitur yang mendukung administrasi pendaftaran dengan efisien." />
+    <meta name="twitter:image" content="social-image.png" />
+    <meta name="twitter:card" content="summary_large_image" />
+
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="{{ asset('template-landpage/img/logo_mi.png') }}" />
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet" />
+    <!-- CSS -->
+
+    @yield('this-page-style') <!-- Menyertakan Style tambahan dari halaman -->
+
+    <link rel="stylesheet" href="{{ asset('template-landpage/css/linearicons.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template-landpage/css/font-awesome.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template-landpage/css/bootstrap.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template-landpage/css/magnific-popup.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template-landpage/css/nice-select.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template-landpage/css/animate.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template-landpage/css/owl.carousel.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template-landpage/css/jquery-ui.css') }}" />
+    <link rel="stylesheet" href="{{ asset('template-landpage/css/main.css') }}" />
+</head>
+
+<body>
+
+    @php
+        $currentDate = \Carbon\Carbon::now();
+        $activeYear = \App\Helpers\AppHelper::getActiveAcademicYear();
+        $activeContact = \App\Helpers\AppHelper::getKontak();
+        $activeAbout = \App\Helpers\AppHelper::getTentang();
+        $activeSetting = \App\Helpers\AppHelper::getSettingPendaftaran();
+    @endphp
+
+    @include('layouts-landpage.header')
+
+    @yield('content')
+
+    @include('layouts-landpage.footer')
+
+    <script src="{{ asset('template-landpage/js/vendor/jquery-2.2.4.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="{{ asset('template-landpage/js/vendor/bootstrap.min.js') }}"></script>
+    <!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script> -->
+    <script src="{{ asset('template-landpage/js/easing.min.js') }}"></script>
+    <script src="{{ asset('template-landpage/js/hoverIntent.js') }}"></script>
+    <script src="{{ asset('template-landpage/js/superfish.min.js') }}"></script>
+    <script src="{{ asset('template-landpage/js/jquery.ajaxchimp.min.js') }}"></script>
+    <script src="{{ asset('template-landpage/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('template-landpage/js/jquery.tabs.min.js') }}"></script>
+    <script src="{{ asset('template-landpage/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('template-landpage/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('template-landpage/js/mail-script.js') }}"></script>
+    <script src="{{ asset('template-landpage/js/main.js') }}"></script>
+
+    @yield('this-page-scripts') <!-- Menyertakan JS tambahan dari halaman -->
+</body>
+
+</html>
---
+++ resources/views/layouts-landpage/header.blade.php
@@ -0,0 +1,71 @@
+<header id="header" id="home">
+    <div class="header-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-sm-6 col-8 header-top-left no-padding">
+                    <ul>
+                        <li>
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 col-sm-6 col-4 header-top-right no-padding">
+                    <a href="tel: {{ $activeContact ? $activeContact->kontak_telepon : '' }}"><span
+                            class="lnr lnr-phone-handset"></span>
+                        <span class="text">
+                            {{ $activeContact ? $activeContact->kontak_telepon : 'Belum ada data' }}</span></a>
+                    <a href="mailto: {{ $activeContact ? $activeContact->kontak_email : '' }}"><span
+                            class="lnr lnr-envelope"></span>
+                        <span class="text">
+                            {{ $activeContact ? $activeContact->kontak_email : 'Belum ada data' }}</span></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container main-menu">
+        <div class="row align-items-center justify-content-between d-flex">
+            <div id="logo">
+                <a href="beranda.html" class="d-flex align-items-center text-white text-decoration-none">
+                    <img src="{{ asset('template-landpage/img/logo_mi_new.png') }}"
+                        alt="Logo Madrasah Ibtidaiyah Ihyauddiniyah" style="max-width: 50px; margin-right: 10px" />
+                    <h6 class="mb-0 text-white">MI Ihyauddiniyah</h6>
+                </a>
+            </div>
+
+            <nav id="nav-menu-container">
+                <ul class="nav-menu">
+                    <li><a href="beranda.html">Beranda</a></li>
+                    <li class="menu-has-children">
+                        <a href="">Informasi</a>
+                        <ul>
+                            <li><a href="profil.html">Profil</a></li>
+                            <li><a href="visi.html">Visi Misi</a></li>
+                            <li>
+                                <a href="ppdb.html">Pendaftaran Peserta Didik Baru
+                                    (PPDB)</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="menu-has-children">
+                        <a href="">Media</a>
+                        <ul>
+                            <li><a href="berita.html">Berita</a></li>
+                            <li>
+                                <a href="galeri.html">Galeri Foto</a>
+                            </li>
+                            <li>
+                                <a href="prestasi.html">Prestasi</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="kontak.html">Kontak</a></li>
+                </ul>
+            </nav>
+            <!-- #nav-menu-container -->
+        </div>
+    </div>
+</header>
+<!-- #header -->
