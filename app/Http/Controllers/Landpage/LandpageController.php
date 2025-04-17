8b4a2b657d241aefbd2de1fd4a469bd59fb4e596
LuthfiRobit <luthfirobit@gmail.com>
2025-04-17 20:13:22 +0700

    mod-view destination

--- app/Http/Controllers/Landpage/LandpageController.php
+++ app/Http/Controllers/Landpage/LandpageController.php
@@ -9,7 +9,7 @@ class LandpageController extends Controller
 {
     public function index()
     {
-        return view('landpage.home.views.index');
+        return view('landpage.beranda.views.index');
     }
 
     public function registration()
