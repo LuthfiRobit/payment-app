e1b66ced0fcb7987512ade9146d8ba17539508da
LuthfiRobit <luthfirobit@gmail.com>
2025-05-05 13:29:33 +0700

    modify- make role case redirect

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,7 +21,20 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                // return redirect(RouteServiceProvider::HOME);
+                $user = Auth::guard($guard)->user();
+                // Redirect berdasarkan role user
+                switch ($user->role) {
+                    case 'petugas_emis':
+                        return redirect()->route('main.dashboard-ppdb.index');
+                    case 'kepsek':
+                    case 'petugas_pembayaran':
+                    case 'bendahara':
+                    case 'developer':
+                        return redirect()->route('main.dashboard.index');
+                    default:
+                        return redirect('/'); // Fallback jika role tidak dikenali
+                }
             }
         }
 
