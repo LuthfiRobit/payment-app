d2d97c07372c7ba72ef49b6c01dbf34f4251ab5e
LuthfiRobit <luthfirobit@gmail.com>
2025-05-05 13:34:58 +0700

    modify- add menu validation based on role auth

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,111 +1,131 @@
 <!-- Sidebar start -->
 <div class="deznav">
     <div class="deznav-scroll">
-        <!-- Sidebar menu -->
+        @php
+            $role = auth()->user()->role;
+        @endphp
+
         <ul class="metismenu" id="menu">
-            <!-- Main section -->
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void(0);" aria-expanded="false">
-                    <i class="fas fa-chart-line fw-bold"></i>
-                    <span class="nav-text">Main</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="{{ route('main.dashboard.index') }}" class="fs-6">Dashboard Pembayaran</a></li>
-                    <li><a href="{{ route('main.dashboard-ppdb.index') }}" class="fs-6">Dashboard PPDB</a></li>
-                </ul>
-            </li>
-            <!-- Payment section -->
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void(0);" aria-expanded="false">
-                    <i class="fas fa-money-check-alt fw-bold"></i>
-                    <span class="nav-text">Transaksi</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li>
-                        <a href="{{ route('transaksi.pembayaran.index') }}" class="fs-6">Pembayaran</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('transaksi.laporan.index') }}" class="fs-6">Laporan</a>
-                    </li>
-                    <li>
-                        <a href="{{ route('transaksi.setor.keuangan.index') }}" class="fs-6">Setor Keuangan</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void(0);" aria-expanded="false">
-                    <i class="fas fa-money-bill-wave fw-bold"></i>
-                    <span class="nav-text">Tagihan</span>
-                </a>
-                @php
-                    $activeYear = \App\Helpers\AppHelper::getActiveAcademicYear();
-                @endphp
-                <ul aria-expanded="false">
-                    <li><a href="{{ route('tagihan.generate-tagihan.index') }}" class="fs-6">Generate tagihan</a>
-                    </li>
-                    <li><a href="{{ route('tagihan.daftar-tagihan.index') }}" class="fs-6">List tagihan
-                            {{ $activeYear ? $activeYear->tahun . '-' . $activeYear->semester : 'Tidak ada' }}
-                        </a>
-                    </li>
-                    <li><a href="{{ route('tagihan.riwayat-tagihan.index') }}" class="fs-6">Riwayat tagihan</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void(0);" aria-expanded="false">
-                    <i class="fas fa-gear fw-bold"></i>
-                    <span class="nav-text">Setting</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="{{ route('setting.tagihan-siswa.index') }}" class="fs-6">Tagihan siswa</a></li>
-                    <li><a href="{{ route('setting.potongan-siswa.index') }}" class="fs-6">Potongan siswa</a></li>
-                </ul>
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void(0);" aria-expanded="false">
-                    <i class="fas fa-server fw-bold"></i>
-                    <span class="nav-text">Master</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="{{ route('master-data.tahun-akademik.index') }}" class="fs-6">Tahun akademik</a>
-                    </li>
-                    <li><a href="{{ route('master-data.iuran.index') }}" class="fs-6">Iuran</a></li>
-                    <li><a href="{{ route('master-data.potongan.index') }}" class="fs-6">Potongan</a></li>
-                    <li><a href="{{ route('master-data.siswa.index') }}" class="fs-6">Siswa</a></li>
+            {{-- MAIN --}}
+            @if (in_array($role, ['developer', 'kepsek', 'bendahara', 'petugas_pembayaran', 'petugas_emis']))
+                <li>
+                    <a class="has-arrow ai-icon" href="javascript:void(0);">
+                        <i class="fas fa-chart-line fw-bold"></i><span class="nav-text">Main</span>
+                    </a>
+                    <ul>
+                        @if (in_array($role, ['developer', 'kepsek', 'bendahara', 'petugas_pembayaran']))
+                            <li><a href="{{ route('main.dashboard.index') }}">Dashboard Pembayaran</a></li>
+                        @endif
+                        @if (in_array($role, ['developer', 'kepsek', 'petugas_emis']))
+                            <li><a href="{{ route('main.dashboard-ppdb.index') }}">Dashboard PPDB</a></li>
+                        @endif
+                    </ul>
+                </li>
+            @endif
 
-                    <li><a href="{{ route('master-data.kontak.index') }}" class="fs-6">CMS Kontak</a></li>
+            {{-- TRANSAKSI --}}
+            @if (in_array($role, ['developer', 'petugas_pembayaran', 'kepsek', 'bendahara']))
+                <li>
+                    <a class="has-arrow ai-icon" href="javascript:void(0);">
+                        <i class="fas fa-money-check-alt fw-bold"></i><span class="nav-text">Transaksi</span>
+                    </a>
+                    <ul>
+                        @if (in_array($role, ['developer', 'petugas_pembayaran']))
+                            <li><a href="{{ route('transaksi.pembayaran.index') }}">Pembayaran</a></li>
+                        @endif
+                        @if (in_array($role, ['developer', 'petugas_pembayaran', 'kepsek']))
+                            <li><a href="{{ route('transaksi.laporan.index') }}">Laporan</a></li>
+                        @endif
+                        @if (in_array($role, ['developer', 'kepsek', 'bendahara']))
+                            <li><a href="{{ route('transaksi.setor.keuangan.index') }}">Setor Keuangan</a></li>
+                        @endif
+                    </ul>
+                </li>
+            @endif
 
-                    <li><a href="{{ route('master-data.tentang.index') }}" class="fs-6">CMS Tentang</a></li>
+            {{-- TAGIHAN --}}
+            @if (in_array($role, ['developer', 'petugas_pembayaran', 'kepsek']))
+                <li>
+                    <a class="has-arrow ai-icon" href="javascript:void(0);">
+                        <i class="fas fa-money-bill-wave fw-bold"></i><span class="nav-text">Tagihan</span>
+                    </a>
+                    @php $activeYear = \App\Helpers\AppHelper::getActiveAcademicYear(); @endphp
+                    <ul>
+                        @if (in_array($role, ['developer', 'petugas_pembayaran']))
+                            <li><a href="{{ route('tagihan.generate-tagihan.index') }}">Generate tagihan</a></li>
+                        @endif
+                        <li><a href="{{ route('tagihan.daftar-tagihan.index') }}">List tagihan
+                                {{ $activeYear ? $activeYear->tahun . '-' . $activeYear->semester : 'Tidak ada' }}</a>
+                        </li>
+                        <li><a href="{{ route('tagihan.riwayat-tagihan.index') }}">Riwayat tagihan</a></li>
+                    </ul>
+                </li>
+            @endif
 
-                    <li><a href="{{ route('master-data.galeri.index') }}" class="fs-6">CMS Galeri</a></li>
+            {{-- SETTING --}}
+            @if (in_array($role, ['developer', 'petugas_pembayaran', 'kepsek']))
+                <li>
+                    <a class="has-arrow ai-icon" href="javascript:void(0);">
+                        <i class="fas fa-gear fw-bold"></i><span class="nav-text">Setting</span>
+                    </a>
+                    <ul>
+                        <li><a href="{{ route('setting.tagihan-siswa.index') }}">Tagihan siswa</a></li>
+                        <li><a href="{{ route('setting.potongan-siswa.index') }}">Potongan siswa</a></li>
+                    </ul>
+                </li>
+            @endif
 
-                    <li><a href="{{ route('master-data.prestasi.index') }}" class="fs-6">CMS Prestasi</a></li>
+            {{-- MASTER --}}
+            @if (in_array($role, ['developer', 'petugas_pembayaran', 'kepsek', 'petugas_emis']))
+                <li>
+                    <a class="has-arrow ai-icon" href="javascript:void(0);">
+                        <i class="fas fa-server fw-bold"></i><span class="nav-text">Master</span>
+                    </a>
+                    <ul>
+                        @if (in_array($role, ['developer', 'petugas_pembayaran', 'kepsek']))
+                            <li><a href="{{ route('master-data.tahun-akademik.index') }}">Tahun akademik</a></li>
+                            <li><a href="{{ route('master-data.iuran.index') }}">Iuran</a></li>
+                            <li><a href="{{ route('master-data.potongan.index') }}">Potongan</a></li>
+                            <li><a href="{{ route('master-data.siswa.index') }}">Siswa</a></li>
+                        @endif
+                        @if (in_array($role, ['developer', 'kepsek', 'petugas_emis']))
+                            <li><a href="{{ route('master-data.kontak.index') }}">CMS Kontak</a></li>
+                            <li><a href="{{ route('master-data.tentang.index') }}">CMS Tentang</a></li>
+                            <li><a href="{{ route('master-data.galeri.index') }}">CMS Galeri</a></li>
+                            <li><a href="{{ route('master-data.prestasi.index') }}">CMS Prestasi</a></li>
+                            <li><a href="{{ route('master-data.berita.index') }}">CMS Berita</a></li>
+                        @endif
+                    </ul>
+                </li>
+            @endif
 
-                    <li><a href="{{ route('master-data.berita.index') }}" class="fs-6">CMS Berita</a></li>
-                </ul>
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void(0);" aria-expanded="false">
-                    <i class="fas fa-id-card fw-bold"></i>
-                    <span class="nav-text">PPDB</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="{{ route('ppdb.create') }}" class="fs-6">Tambah data siswa</a></li>
-                    <li><a href="{{ route('ppdb.index') }}" class="fs-6">List data siswa</a></li>
+            {{-- PPDB --}}
+            @if (in_array($role, ['developer', 'kepsek', 'petugas_emis']))
+                <li>
+                    <a class="has-arrow ai-icon" href="javascript:void(0);">
+                        <i class="fas fa-id-card fw-bold"></i><span class="nav-text">PPDB</span>
+                    </a>
+                    <ul>
+                        <li><a href="{{ route('ppdb.create') }}">Tambah data siswa</a></li>
+                        <li><a href="{{ route('ppdb.index') }}">List data siswa</a></li>
+                        <li><a href="{{ route('ppdb.setting.index') }}">Setting</a></li>
+                    </ul>
+                </li>
+            @endif
 
-                    <li><a href="{{ route('ppdb.setting.index') }}" class="fs-6">Setting</a></li>
-                </ul>
-            </li>
-            <li>
-                <a class="has-arrow ai-icon" href="javascript:void(0);" aria-expanded="false">
-                    <i class="fas fa-cogs fw-bold"></i> <!-- Ganti dengan ikon yang lebih sesuai untuk aplikasi -->
-                    <span class="nav-text">Aplikasi</span>
-                </a>
-                <ul aria-expanded="false">
-                    <li><a href="{{ route('application.user.index') }}" class="fs-6">Manajemen Pengguna</a></li>
-                </ul>
-            </li>
+            {{-- APLIKASI --}}
+            @if (in_array($role, ['developer', 'kepsek']))
+                <li>
+                    <a class="has-arrow ai-icon" href="javascript:void(0);">
+                        <i class="fas fa-cogs fw-bold"></i><span class="nav-text">Aplikasi</span>
+                    </a>
+                    <ul>
+                        <li><a href="{{ route('application.user.index') }}">Manajemen Pengguna</a></li>
+                    </ul>
+                </li>
+            @endif
         </ul>
+
 
         <!-- Footer with copyright information -->
         <div class="copyright">
